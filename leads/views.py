2913c7720aa87db928d691ffdf047ac784341151
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-08 12:29:36 -0300

    add list and details views for leads app

--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Lead
+
+
+def lead_list(request):
+    leads = Lead.objects.all()
+    context = {
+        'leads': leads
+    }
+    return render(request, 'leads/lead_list.html', context=context)
+
+
+def lead_detail(request, pk):
+    lead = Lead.objects.get(id=pk)
+    context = {
+        'lead': lead
+    }
+    return render(request, 'leads/lead_detail.html', context=context)
