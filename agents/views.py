4643506ececfeed23e7c24680d42ed08500455b7
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-08 18:18:56 -0300

    add list and create agents views

---
+++ agents/views.py
@@ -0,0 +1,28 @@
+from django.views import generic
+from django.shortcuts import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from leads.models import Agent
+from .forms import AgentModelForm
+
+
+class AgentListView(LoginRequiredMixin, generic.ListView):
+    template_name = 'agents/agent_list.html'
+    context_object_name = 'agents'
+
+    def get_queryset(self):
+        return Agent.objects.all()
+
+
+class AgentCreateView(LoginRequiredMixin, generic.CreateView):
+    template_name = 'agents/agent_create.html'
+    form_class = AgentModelForm
+
+    def get_success_url(self):
+        return reverse('agents:agent-list')
+
+    def form_valid(self, form):
+        agent = form.save(commit=False)
+        agent.organization = self.request.user.userprofile
+        agent.save()
+        return super(AgentCreateView, self).form_valid(form)
