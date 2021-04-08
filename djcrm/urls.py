d77fd413c627e28d2dd2d6357564c4eff46bd957
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-08 16:03:10 -0300

    add support for static files

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 
 from leads.views import LandingPageView
 
@@ -9,3 +11,6 @@ urlpatterns = [
     path('', LandingPageView.as_view(), name='landing-page'),
     path('leads/', include('leads.urls', namespace='leads'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
