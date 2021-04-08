5da3a328036cbf054a4167487c36e2ec49411748
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-08 13:02:07 -0300

    refactor lead form to use lead models as a base

--- leads/views.py
+++ leads/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
-from .forms import LeadForm
-from .models import Agent, Lead
+from .forms import LeadModelForm
+from .models import Lead
 
 
 def lead_list(request):
@@ -21,20 +21,11 @@ def lead_detail(request, pk):
 
 
 def lead_create(request):
-    form = LeadForm()
+    form = LeadModelForm()
     if request.method == 'POST':
-        form = LeadForm(request.POST)
+        form = LeadModelForm(request.POST)
         if form.is_valid():
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            age = form.cleaned_data['age']
-            agent = Agent.objects.first()
-            Lead.objects.create(
-                first_name=first_name,
-                last_name=last_name,
-                age=age,
-                agent=agent
-            )
+            form.save()
             return redirect('/leads')
     context = {
         'form': form
