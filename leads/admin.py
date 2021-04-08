51eebf77a887a49ab9526d057db090c9c57e08a2
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-08 11:49:14 -0300

    add initial models and register them to admin site

--- leads/admin.py
+++ leads/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Agent, Lead, User
+
+
+admin.site.register(Agent)
+admin.site.register(Lead)
+admin.site.register(User)
