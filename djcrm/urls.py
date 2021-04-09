d35358ae90a3737a558153cc8b36d9181b14f518
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-08 22:10:12 -0300

    add password reset view

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -2,7 +2,14 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 
 from leads.views import LandingPageView, SignupView
 
@@ -13,9 +20,17 @@ urlpatterns = [
     path('leads/', include('leads.urls', namespace='leads')),
     path('agents/', include('agents.urls', namespace='agents')),
     path('signup/', SignupView.as_view(), name='signup'), 
+    path('reset-password/', PasswordResetView.as_view(), name='reset-password'),
+    path('password-reset-done/',
+        PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>',
+        PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/',
+        PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT)
