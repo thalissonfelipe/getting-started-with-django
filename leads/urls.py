810805ee28d912d59b299b544bd144672253f362
Thalisson Felipe <thalissonlipe7@gmail.com>
2021-04-09 15:24:40 -0300

    add categories detail view

--- leads/urls.py
+++ leads/urls.py
@@ -7,7 +7,8 @@ from .views import (
     LeadUpdateView,
     LeadDeleteView,
     AssignAgentView,
-    CategoryListView
+    CategoryListView,
+    CategoryDetailView
 )
 
 app_name = 'leads'
@@ -17,7 +18,10 @@ urlpatterns = [
     path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
     path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
     path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
-    path('<int:pk>/assign-agent/', AssignAgentView.as_view(), name='assign-agent'),
+    path('<int:pk>/assign-agent/',
+        AssignAgentView.as_view(), name='assign-agent'),
     path('create/', LeadCreateView.as_view(), name='lead-create'),
-    path('categories', CategoryListView.as_view(), name='category-list')
+    path('categories', CategoryListView.as_view(), name='category-list'),
+    path('categories/<int:pk>/',
+        CategoryDetailView.as_view(), name='category-detail')
 ]
--- leads/views.py
+++ leads/views.py
@@ -169,6 +169,29 @@ class CategoryListView(LoginRequiredMixin, ListView):
         return queryset
 
 
+class CategoryDetailView(DetailView):
+    template_name = 'leads/category_detail.html'
+    context_object_name = 'category'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(CategoryDetailView, self).get_context_data(**kwargs)
+    #     leads = self.get_object().leads.all()
+    #     context.update({'leads': leads})
+    #     return context
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_organizor:
+            queryset = Category.objects.filter(
+                organization=user.userprofile
+            )
+        else:
+            queryset = Category.objects.filter(
+                organization=user.agent.organization
+            )
+        return queryset
+
+
 # def landing_page(request):
 #     return render(request, 'landing.html')
 
